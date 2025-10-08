2a215e6198a0f142313cd0f5df9eb5893a297bd8
Mohamed <ma770106@gmail.com>
2025-10-08 15:35:15 +0300

    Commit

--- LibrarySystem/Areas/Admin/Controllers/BookController.cs
+++ LibrarySystem/Areas/Admin/Controllers/BookController.cs
@@ -28,11 +28,31 @@ namespace LibrarySystem.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Book book)
+        public IActionResult Create(Book book,IFormFile ImageUrl)
         {
-            _context.Books.Add(book);
-            _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
+          
+            if (ImageUrl is not null && ImageUrl.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(ImageUrl.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images",
+                    fileName);
+
+                //save Img in wwwroot
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    ImageUrl.CopyTo(stream);
+                }
+
+                //save img name in db
+                book.ImageUrl = fileName;
+
+                //Save In Db
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return BadRequest();
+               
         }
 
         [HttpGet]
@@ -46,9 +66,77 @@ namespace LibrarySystem.Areas.Admin.Controllers
             return View(book);
         }
         [HttpPost]
-        public IActionResult Edit(Book book)
+        public IActionResult Edit(Book book, IFormFile? ImageUrl)
         {
+            //عشان لو مرفعتش صورة ميضربش ايرور يروح يجيب الصورة اللي مرفوعة قبل كدا من الداتابيز
+            var BookInDb = _context.Books.AsNoTracking().FirstOrDefault(e => e.Id == book.Id);
+            if (BookInDb is null)
+            {
+                return BadRequest();
+            }
+
+
+            if (ImageUrl is not null && ImageUrl.Length > 0)
+            {
+                // إنشاء اسم فريد للصورة الجديدة
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(ImageUrl.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images",
+                    fileName);
+
+                //save Img in wwwroot
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    ImageUrl.CopyTo(stream);
+                }
+
+                //delete old img from wwwroot
+                // حذف الصورة القديمة (إن وجدت)
+
+                if (!string.IsNullOrEmpty(BookInDb.ImageUrl))
+                {
+                    var OldFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images",
+                   BookInDb.ImageUrl);
+                    if (System.IO.File.Exists(OldFilePath))
+                    {
+                        System.IO.File.Delete(OldFilePath);
+                    }
+
+                }
+                //update img name in db
+                book.ImageUrl = fileName;
+            }
+            else
+            {
+                // لو المستخدم ما رفعش صورة، نحافظ على القديمة
+
+                book.ImageUrl = BookInDb.ImageUrl;
+            }
+        //update in db
             _context.Books.Update(book);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+       
+
+        public IActionResult Delete(int id)
+        {
+            var book = _context.Books.FirstOrDefault(e => e.Id == id);
+            if (book is null)
+            {
+                return RedirectToAction(SD.NotFoundPage, SD.HomeController);
+            }
+            //delete old img from wwwroot
+
+            var OldFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images",
+               book.ImageUrl);
+            if (System.IO.File.Exists(OldFilePath))
+            {
+                System.IO.File.Delete(OldFilePath);
+            }
+
+
+            //Remove in db
+            _context.Books.Remove(book);
             _context.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
